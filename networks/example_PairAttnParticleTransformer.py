b1074873e4552e6c236dbaf4c3017d2ba187b97b
Javier Duarte <jduarte@ucsd.edu>
2024-08-22 21:59:06 +0000

    64 dim

--- networks/example_PairAttnParticleTransformer.py
+++ networks/example_PairAttnParticleTransformer.py
@@ -31,7 +31,7 @@ def get_model(data_config, **kwargs):
         use_pre_activation_pair=False,
         embed_dims=[128, 512, 128],
         pair_embed_dims=[64, 64, 64],
-        num_heads=8,
+        num_heads=64,
         num_layers=8,
         num_cls_layers=2,
         block_params={'attn_type': 'pairs'},
